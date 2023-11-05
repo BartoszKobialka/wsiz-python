47b489ce7471429ea59061e808ddb69f39ff1afb
Bartosz Kobiałka <w69797@student.wsiz.edu.pl>
2023-11-05 17:13:22 +0100

    [feat] Lab-0-ex-4

--- Lab0/labMain.py
+++ Lab0/labMain.py
@@ -1,6 +1,20 @@
 #3
+def ex3():
+    x = int(input("Podaj dlugosc: "))
+    y = int(input("Podaj wysokosc: "))
 
-x = int(input("Podaj dlugosc: "))
-y = int(input("Podaj wysokosc: "))
+    print(f'Pole: {x*y} Obwod: {2*(x+y)}')
 
-print(f'Pole: {x*y} Obwod: {2*(x+y)}')
+#4
+def ex4():
+    distance = int(input("Podaj dlugosc trasy: "))
+    avgFuelUsage = float(input("Podaj srednie spalanie: "))
+
+    fuelUsed = avgFuelUsage * distance / 100
+
+    print(f'Spalono: {fuelUsed}l \n Przewidywany koszt {(fuelUsed * 6.5):.2f}zł')
+
+#RUN
+# ex3()
+ex4()
+
