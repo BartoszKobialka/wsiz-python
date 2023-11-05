500a0f8f70665a2fe9a4ab8c06e3060aa48f82bf
Bartosz Kobialka <w69797@student.wsiz.edu.pl>
2023-11-05 17:45:45 +0100

    [feat] Lab-1-ex-2

--- Lab1/labMain.py
+++ Lab1/labMain.py
@@ -3,12 +3,23 @@ def ex1():
     age = int(input("Podaj wiek: "))
     price = 0
 
-    if age >= 4 and age <= 18:
+    if 4 <= age <= 18:
         price = 10
     elif age > 18:
         price = 20
 
     print(f"Cena biletu: {price}zl")
+
+#2
+def ex2():
+    char = chr(input("Podaj znak"))
+
+    if chr('a') <= char <= chr('z'):
+        print("podano mala litere")
+    elif chr('A') <= char <= chr('Z'):
+        print("podano duza litere")
+    else:
+        print("Nie podano litery")
 
 
 #RUN
