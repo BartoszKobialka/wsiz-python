91c29879cf1b554f23cf1a36838bccd27733a13e
Bartosz Kobialka <bartekkobialka1@gmail.com>
2023-11-24 20:50:57 +0100

    [feat] Lab-1 helpers

---
+++ Lab1/eEx1Helpers.py
@@ -0,0 +1,21 @@
+def a(x):
+    if x > 0:
+        return 2*x
+    if x == 0:
+        return 0
+    
+    return -3*x
+
+def b(x):
+    if x >= 1:
+        return x**2
+    
+    return x
+
+def c(x):
+    if x > 2:
+        return 2 + x
+    if x == 2:
+        return 8
+    
+    return x-4
